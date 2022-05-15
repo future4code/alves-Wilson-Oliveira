7f202f42626062680b692db493d8c319fe392603
Wilson Santos de OLiveira <w.s.oliveira12@hotmail.com>
2022-05-15 09:59:12 -0300

    projeto-lista-ls

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -18,8 +18,8 @@ function imprimeMensagem() {
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
-  const area1=(+prompt("digite a altura"))*(+prompt("digite a largura"))
-  console.log (area1)
+ 
+  console.log ((+prompt("digite a altura"))*(+prompt("digite a largura")))
 
 }
 
@@ -28,15 +28,15 @@ function calculaAreaRetangulo() {
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
-  const idade1=(+prompt("insira o ano atual")-(+prompt("insira seu ano de nacimento")))
-  console.log(idade1)
+  
+  console.log ((+prompt("insira o ano atual"))-(+prompt("insira seu ano de nacimento")))
 
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  const altura2=(altura*altura)
-  return peso/altura2
+
+  return peso/(altura*altura)
 
 }
 
@@ -100,18 +100,11 @@ function retornaUltimoElemento(array) {
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
   const array0=array[0]
-  const array1= array[0]=array[array.length-1]
-  const array12=array[array.length-1]=array0
+  array[0]=array[array.length-1]
+  array[array.length-1]=array0
 
   return array
 
-  
- 
-
-
-
-
-  
 }
 
 // EXERCÍCIO 12
@@ -124,28 +117,43 @@ function checaIgualdadeDesconsiderandoCase(string1, string2) {
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  const anoAtual=+prompt("Qual o ano atual?")
+
+const anoAtual=+prompt("Qual o ano atual?")
 const anoDeNascimento= +prompt("Qual o ano do seu nascimento?")
 const anoDoRG=+prompt("Qual ano foi expedido seu rg?")
 const idadeDoUsuario=(anoAtual-anoDeNascimento)
 const idadeDoRG=(anoAtual-anoDoRG)
 const renovação=(idadeDoUsuario>=50)
+const renovação2=(idadeDoUsuario<50)
+const renovação3=(idadeDoUsuario>20)
 const maiorDe50=(idadeDoRG>=15)
 const menorDe50=(idadeDoRG>=10)
-if (renovação) {console.log(maiorDe50)} else {console.log(menorDe50)}
+const menorDe20=(idadeDoRG>=5)
+if (renovação) {console.log(maiorDe50)} 
+if(renovação2&&renovação3 ){console.log(menorDe50)}
+if(renovação3){} else{console.log(menorDe20)}
 
 }
 
 
-
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
+const anoCondicao1 =(ano%400==0)
+const anoCondicao2=(ano%4==0&& ano%100!=0)
+if (anoCondicao2){return anoCondicao2}else{return anoCondicao1}
+
+  
 
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
+  const idadeLabenu=prompt("Você tem mais de 18 anos?").toLowerCase()===("sim")
+  const escolaridade=prompt("Você possui ensino médio completo?").toLowerCase()===("sim")
+  const disponibilidade=prompt("Você possui disponibilidade exclusiva durante os horários do curso?").toLowerCase()===("sim")
+  const condicoesLabenu=idadeLabenu&&escolaridade&&disponibilidade
+  console.log(condicoesLabenu)
+
   // implemente sua lógica aqui
 
 }
