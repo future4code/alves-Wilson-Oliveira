d5b39ecafface9d230bf155f55ef06354185f854
wilsonsantos1992 <w.s.oliveira12@hotmail.com>
2022-08-17 20:48:28 -0300

    fluxo

---
+++ Modulo-4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,17 @@
+export let produtos: {id:string,name:string,price:number}[]=[
+  {
+    id:"1",
+    name:"pão",
+    price: 2
+  },{
+    id:"2",
+    name:"arroz",
+    price: 10
+  },
+  {
+    id:"3",
+    name:"batata",
+    price: 1
+  }
+
+ ]
